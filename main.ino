02d8de4f9af5ee0613f47cc92a61e077e073c7be
zxc469469 <zxc469469@gmail.com>
2017-12-29 16:17:05 +0800

    12/29 功能完成,還沒demo

--- main.ino
+++ main.ino
@@ -1,3 +1,5 @@
+#include <softwareserial.h>
+#include <dfplayer_mini_mp3.h>  
 
 //六個燈在shift register的狀態
 const char LED[7]={0x01,0x02,0x04,0x08,0x10,0x20,0x00};
@@ -10,13 +12,15 @@ const char Button[6]={3,4,5,6,7,8};
 
 
 void setup(){
+    Serial.begin (9600);
     for(i=0;i<sizeof(Button);i++){
         pinMode(Button[i],INPUT);
     }
     pinMode(dataPin,OUTPUT);
     pinMode(clockPin,OUTPUT);
     pinMode(latchPin,OUTPUT);
-
+    mp3_set_serial (Serial); 
+    mp3_set_volume (10);
 
 }
 
@@ -27,12 +31,10 @@ void loop()
     for(i=0;i<sizeof(Button);i++){
         if(digitalRead(Button[i])==HIGH){
             shiftOut(dataPin, clockPin, LSBFIRST,LED[i]);   //從左邊讀取LED[i]的資料 
-            
+            mp3_play (i);
+            shiftOut(dataPin, clockPin, LSBFIRST,LED[6]);
+            delay(100);
         }
     }   
-    
 }
 
-void PlayMusic(int fileOrder){
-    
-}
