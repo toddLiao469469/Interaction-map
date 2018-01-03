3ba50848620f0b35b621b303bc4ea782c076faf9
zxc469469 <zxc469469@gmail.com>
2018-01-03 21:02:36 +0800

    改寫結構

--- main.ino
+++ main.ino
@@ -1,5 +1,5 @@
-#include <softwareserial.h>
-#include <dfplayer_mini_mp3.h>  
+#include <SoftwareSerial.h>
+#include <DFPlayer_Mini_Mp3.h>
 
 //六個燈在shift register的狀態
 const char LED[7]={0x01,0x02,0x04,0x08,0x10,0x20,0x00};
@@ -8,33 +8,76 @@ const int dataPin = 4;
 const int clockPin = 2;
 const int latchPin  = 3;
 //六個按鈕
-const char Button[6]={3,4,5,6,7,8};
+const char Button[6]={5,6,7,8,9,10};
 
+const char rxpin = 12;
+const char txpin = 11;
+SoftwareSerial mySerial(rxpin, txpin); // RX, TX
 
 void setup(){
-    Serial.begin (9600);
-    for(i=0;i<sizeof(Button);i++){
+    pinMode(rxpin, INPUT);
+    pinMode(txpin, OUTPUT);
+    mySerial.begin (9600);
+    mp3_set_serial (mySerial);  //set softwareSerial for DFPlayer-mini mp3 module 
+    Serial.begin(115200);
+    
+    for(int i=0;i<sizeof(Button);i++){
         pinMode(Button[i],INPUT);
     }
     pinMode(dataPin,OUTPUT);
     pinMode(clockPin,OUTPUT);
     pinMode(latchPin,OUTPUT);
-    mp3_set_serial (Serial); 
-    mp3_set_volume (10);
 
+    mp3_set_volume (30);
 }
 
-
-
-void loop()
-{
-    for(i=0;i<sizeof(Button);i++){
-        if(digitalRead(Button[i])==HIGH){
-            shiftOut(dataPin, clockPin, LSBFIRST,LED[i]);   //從左邊讀取LED[i]的資料 
-            mp3_play (i);
-            shiftOut(dataPin, clockPin, LSBFIRST,LED[6]);
+void loop(){
+    for(int Order=0;Order<6;Order++){
+        int mp3Order = Order + 1 ;
+        if(digitalRead(Button[Order] == HIGH)){
+            
+            
+            LEDPlay(Order)
+            MP3Play(mp3Order);
+            LEDClose();
+            
             delay(100);
         }
     }   
 }
 
+void LEDPlay(int Order){
+    digitalWrite(latchPin, LOW);
+    shiftOut(dataPin, clockPin, MSBFIRST, LED[Order]);
+    digitalWrite(latchPin, HIGH); 
+}
+
+void LEDClose(void){
+    digitalWrite(latchPin, LOW);
+    shiftOut(dataPin, clockPin, MSBFIRST, LED[]);
+    digitalWrite(latchPin, HIGH); 
+}
+
+void MP3Play(int Order){
+    switch(Order){
+        mp3_play(Order);
+            case 1:
+                delay(10000);
+                break;
+            case 2:
+                delay(10000);
+                break;
+            case 3:
+                delay(10000);
+                break;
+            case 4:
+                delay(10000);
+                break;
+             case 5:
+                delay(10000);
+                break;
+            case 6:
+                delay(10000);
+                break;
+    }
+}
